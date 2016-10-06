5d11d33a2af8ae47d3ba3462cbfb61689f9fe6bc
Filippo Giuliani <fgiuliani@pcfilippo.giuliani>
2016-10-06 21:53:27 +0200

    Modificati header

--- kernel.py
+++ kernel.py
@@ -1,19 +1,79 @@
 #!/usr/bin/python
+#
+#   Linker Script Generator
+#   
+#   This script generate the linker script to link kembed
+#
+#
+#   $LastChangedDate: 2016-07-22 21:42:37 -0700 (Sat, 22 Jul 2006) $
+#   $Revision: 144 $
+#   $Author: filippogiuliani $
+#   $Id: makefile 148 2006-07-28 21:30:43Z sally $
+#
+#
+#   (C) Copyright 2016 Filippo Giuliani <mail@filippogiuliani.it>
+#
+#   This file is part of kembed.
+#
+#   kembed is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   kembed is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with kembed.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import sys
 
-print ('/******************************************************************************\n'
-       '*	kernel.ld                                                              \n'
-       '*	 by Filippo Giuliani                                                   \n'
-       '*                                                                              \n'
-       '*	A linker script for generation of raspberry pi kernel images.          \n'
-       '******************************************************************************/\n'
+print ('/**                                                                            \n' 
+       ' *    Linker Script for kembed                                                 \n'
+       ' *                                                                             \n'
+       ' *    This is the linker script to link kembed system. This file is auto       \n'
+       ' *    generate by kernel.py (invoked by the makefile).                         \n'
+       ' *                                                                             \n'
+       ' *                                                                             \n'
+       ' *    $LastChangedDate: 2016-07-22 21:42:37 -0700 (Sat, 22 Jul 2006) $         \n'
+       ' *    $Revision: 144 $                                                         \n'
+       ' *    $Author: harry $                                                         \n'
+       ' *    $Id: calc.c 148 2006-07-28 21:30:43Z sally $                             \n'
+       ' *                                                                             \n'
+       ' *                                                                             \n'
+       ' *    (C) Copyright 2016 Filippo Giuliani <mail@filippogiuliani.it>            \n'
+       ' *                                                                             \n'
+       ' *    This file is part of kembed.                                             \n'
+       ' *                                                                             \n'
+       ' *    kembed is free software: you can redistribute it and/or modify           \n'
+       ' *    it under the terms of the GNU General Public License as published by     \n'
+       ' *    the Free Software Foundation, either version 3 of the License, or        \n'
+       ' *    (at your option) any later version.                                      \n'
+       ' *                                                                             \n'
+       ' *    kembed is distributed in the hope that it will be useful,                \n'
+       ' *    but WITHOUT ANY WARRANTY; without even the implied warranty of           \n'
+       ' *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            \n'
+       ' *    GNU General Public License for more details.                             \n'
+       ' *                                                                             \n'
+       ' *    You should have received a copy of the GNU General Public License        \n'
+       ' *    along with kembed.  If not, see <http://www.gnu.org/licenses/>.          \n'
+       ' */                                                                            \n'
+       '                                                                               \n'
+       '                                                                               \n'
        'OUTPUT_FORMAT("elf32-littlearm", "elf32-bigarm",                               \n'
        '          "elf32-littlearm")                                                   \n'
+       '                                                                               \n'
        'OUTPUT_ARCH(arm)                                                               \n'
+       '                                                                               \n'
        'ENTRY(_start)                                                                  \n'
+       '                                                                               \n'
        'SEARCH_DIR("=/usr/local/lib"); SEARCH_DIR("=/lib"); SEARCH_DIR("=/usr/lib");   \n'
+       '                                                                               \n'
        'ENTRYPOINT = 0x8000;                                                           \n'
+       '                                                                               \n'
        'SECTIONS {                                                                     \n'
        '	/*                                                                     \n'
        '	* First and formost we need the .text.startup section, containing the  \n'
